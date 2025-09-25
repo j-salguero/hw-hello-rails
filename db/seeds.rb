509ff0d1d6267a22762d09e153050f3e8df8238f
Jessica Salguero <jsalguer@uoregon.edu>
2025-09-24 20:18:22 -0600

    Part 5. Added 3 new movies. Pushing changes to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,13 @@ more_movies = [
   { title: "Nomadland", rating: "R",
     release_date: "19-Feb-2021" },
   { title: "CODA", rating: "PG-13",
-    release_date: "13-Aug-2021" }
+    release_date: "13-Aug-2021" },
+  { title: "Trick 'r Treat", rating: "R",
+    release_date: "09-Dec-2007" },
+  { title: "10 Things I Hate About You", rating: "PG-13",
+    release_date: "31-March-1999" },
+  { title: "The Devil Weards Prada", rating: "PG-13",
+    release_data: "30-June-2006" } 
 ]
 
 more_movies.each do |movie|
